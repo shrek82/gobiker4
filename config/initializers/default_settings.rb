3b1a708ecf3b5a3fda4151b3d6736c8fd870916a
shrek82 <37294812@qq.com>
2014-04-23 17:10:49 +0800

    update

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -1,5 +1,6 @@
-Setting.site_name='骑趣网'
-Setting.site_url='http://gobiker.cn'
-Setting.author='jiangang.zhao'
-Setting.keyword='杭州骑行,骑行路线'
-Setting.copyright='copyright @gobiker.cn'
+#首次不能能再console中db:migrate创建
+#Setting.site_name='骑趣网'
+#Setting.site_url='http://gobiker.cn'
+#Setting.author='jiangang.zhao'
+#Setting.keyword='杭州骑行,骑行路线'
+#Setting.copyright='copyright @gobiker.cn'
