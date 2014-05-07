59f7a16f29a744dff3034811ab6a6815e57fe6c8
shrek82 <37294812@qq.com>
2014-05-07 09:44:35 +0800

    delete .build and dist temp files

--- app/assets/js/reglogin/.build/latest/reglogin-debug.js
+++
@@ -1,5 +0,0 @@
-define("reglogin/latest/reglogin-debug", [ "lib/latest/lib-debug", "./login-debug", "./register-debug" ], function(require, exports, module) {
-    var lib = require("lib/latest/lib-debug");
-    exports.login = require("./login-debug");
-    exports.register = require("./register-debug");
-});
--- app/assets/js/reglogin/.build/latest/reglogin.js
+++
@@ -1,5 +0,0 @@
-define("reglogin/latest/reglogin", [ "lib/latest/lib", "./login", "./register" ], function(require, exports, module) {
-    var lib = require("lib/latest/lib");
-    exports.login = require("./login");
-    exports.register = require("./register");
-});
